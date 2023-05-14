7ffd828c96e0ddbb09a616a37cebb9a181ac54e3
isurumanod <isurumanod99@gmail.com>
2023-05-14 07:24:53 +0530

    ready

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Tag)
+admin.site.register(Skills)
+admin.site.register(Messages)
+admin.site.register(Comments)
--- portfolio_dev/settings.py
+++ portfolio_dev/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio_dev.urls'
@@ -66,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'whitenoise.middleware.WhiteNoiseMiddleware',
+
             ],
         },
     },
